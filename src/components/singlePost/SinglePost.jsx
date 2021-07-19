135c22f43386d2189f560cc3fdd5fc6990368e8f
digeoesp <diegocrypto@outlook.com>
2021-07-18 20:36:46 -0700

    singlePost css added

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,72 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+    return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                <img
+                    src="https://snowbrains.com/wp-content/uploads/2015/12/0013.jpg"
+                    alt="post"
+                    className="singlePostImg" />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit amet.
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Diego Espinoza</b></span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className="singlePostDesc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur molestiae vero vitae expedita itaque ducimus alias atque rerum
+                    labore quia at ab consequatur dicta earum nobis, delectus aut explicabo architecto.
+                </p>
+            </div>
+        </div>
+    )
+}
